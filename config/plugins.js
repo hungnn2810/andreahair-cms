c998fa735a64384916d3281a4245f2dcb0a60cfc
hungnn2810 <hungnn2810@gmail.com>
2023-08-26 13:32:49 +0700

    up email

---
+++ config/plugins.js
@@ -0,0 +1,22 @@
+module.exports = ({ env }) => ({
+  // ...
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("EMAIL_SMTP_HOST", "smtp.example.com"),
+        port: env("EMAIL_SMTP_PORT", 587),
+        auth: {
+          user: env("EMAIL_SMTP_USER"),
+          pass: env("EMAIL_SMTP_PASS"),
+        },
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: env("EMAIL_ADDRESS_FROM"),
+        defaultReplyTo: env("EMAIL_ADDRESS_REPLY"),
+      },
+    },
+  },
+  // ...
+});
