a0c9d3c14869186a16bb182da781eb108b6373fc
hungnn2810 <hungnn2810@gmail.com>
2023-09-02 20:17:04 +0700

    update mail

--- src/api/order/content-types/order/lifecycles.js
+++ src/api/order/content-types/order/lifecycles.js
@@ -8,7 +8,7 @@ module.exports = {
           .plugin("email")
           .service("email")
           .send({
-            to: "hungnn2810@gmail.com",
+            to: `${event.result.email}`,
             from: "noreply@andreaharivn.com",
             subject: "Order success",
             html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
